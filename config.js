6506e2ee203df3c710d56f586aa3e81e816576ae
shreya2595 <91082487+shreya2595@users.noreply.github.com>
2026-01-31 23:11:27 -0500

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your big warm hug and a huge kiss!",
+        message: "Now come get your big warm hug and a huge puchieee!",
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
